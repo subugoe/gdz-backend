0879145c6564dc40df83bacaafe3594e368830a4
Joerg Panzer <panzer@sub.uni-goettingen.de>
2016-03-02 23:55:03 +0100

    lots of modifications :-(

--- app/models/bibliographic_file_set.rb
+++ app/models/bibliographic_file_set.rb
@@ -1,8 +1,20 @@
 class BibliographicFileSet < ActiveFedora::Base
   include Hydra::Works::FileSetBehavior
-  property :title, predicate: ::RDF::DC.title, multiple: false do |index|
+
+  property :label, predicate: ::RDF::Vocab::DC.title, multiple: false do |index|
     index.as :stored_searchable
   end
+
+  property :logicaltype, predicate: ::RDF::URI.new('http://opaquenamespace.org/hydra/logicalType'), multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :order, predicate: ::RDF::URI.new('http://opaquenamespace.org/hydra/order'), multiple: false do |index|
+    index.as :stored_searchable
+    index.type :integer
+  end
+
+
 
   def page?
     false
--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,6 +1,6 @@
 class Collection < ActiveFedora::Base
   include Hydra::Works::CollectionBehavior
-  property :title, predicate: ::RDF::DC.title, multiple: false do |index|
+  property :title, predicate: ::RDF::Vocab::DC.title, multiple: false do |index|
     index.as :stored_searchable
   end
 end
