44307969cc590e7cd4404d01424e7f8911e926bb
Joerg Panzer <panzer@sub.uni-goettingen.de>
2016-04-15 12:45:04 +0200

    merge dev into master

--- app/models/bibliographic_file_set.rb
+++ app/models/bibliographic_file_set.rb
@@ -2,8 +2,6 @@ class BibliographicFileSet < ActiveFedora::Base
   include Hydra::Works::FileSetBehavior
 
   property :label, predicate: ::RDF::Vocab::DC.title, multiple: false do |index|
-<<<<<<< HEAD
-=======
     index.as :stored_searchable
   end
 
@@ -17,20 +15,8 @@ class BibliographicFileSet < ActiveFedora::Base
   end
 
   property :recordIdentifier, predicate: ::RDF::Vocab::MODS.recordIdentifier, multiple: false do |index|
->>>>>>> dev
     index.as :stored_searchable
   end
-
-  property :logicaltype, predicate: ::RDF::URI.new('http://opaquenamespace.org/hydra/logicalType'), multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :order, predicate: ::RDF::URI.new('http://opaquenamespace.org/hydra/order'), multiple: false do |index|
-    index.as :stored_searchable
-    index.type :integer
-  end
-
-
 
   def page?
     false
