e95d1048294ade1dc96531ac3dc4b3cb219f7541
Joerg Panzer <panzer@sub.uni-goettingen.de>
2016-04-20 12:57:59 +0200

    clean up

--- docker/fcrepo/scripts/runall.sh
+++ docker/fcrepo/scripts/runall.sh
@@ -10,14 +10,9 @@ sed -i "s|#org.ops4j.pax.url.mvn.localRepository=|org.ops4j.pax.url.mvn.localRep
 
 sleep 90
 
+
 /fcrepo_config/fedora_camel_toolbox.sh
 
 
 cd /usr/local/tomcat
 sh /usr/local/tomcat/bin/catalina.sh run
-
-
-echo "zu weit"
-sleep 600
-
-#CMD ["/opt/tomcat/bin/catalina.sh", "run"]
