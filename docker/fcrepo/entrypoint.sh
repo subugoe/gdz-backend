55a59123327a021f18d04b98c32c53885109652b
Joerg Panzer <panzer@sub.uni-goettingen.de>
2016-04-26 13:00:23 +0200

    add log output, set ownership and rights

--- docker/fcrepo/entrypoint.sh
+++ docker/fcrepo/entrypoint.sh
@@ -1,27 +1,51 @@
 #!/bin/bash
 
 
+echo "---whoami --------------------------------"
+whoami
+
+echo "--- ls -l --------------------------------"
+ls -l $CATALINA_HOME/conf
 
 mv /usr/local/tomcat/tomcat-users.xml /usr/local/tomcat/conf/tomcat-users.xml
 
 
-/usr/local/tomcat/bin/catalina.sh run > /dev/null 2>&1 &
+echo "--- ps aux (1) --------------------------------"
+ps aux
+
+
+catalina.sh run &
+#catalina.sh run > /dev/null 2>&1 &
 #/usr/local/tomcat/bin/catalina.sh run &
 
-sleep 600
+echo "--- ps aux (2)  --------------------------------"
+ps aux
+
+
 
 #----------------
 # Install Fedora4
 
+
+
 cd /tmp
 curl -fSL https://github.com/fcrepo4/fcrepo4/releases/download/fcrepo-$FEDORA_TAG/fcrepo-webapp-$FEDORA_VERSION.war -o fcrepo.war
+chown -R $FCREPO_USER:$FCREPO_USER fcrepo.war
 
-mkdir -p /var/lib/tomcat/fcrepo4-data
+#mkdir -p /var/lib/tomcat/fcrepo4-data
 #RUN chown -R $FCREPO_UID /var/lib/tomcat/fcrepo4-data
 #	&& chmod g-w /var/lib/tomcat/fcrepo4-data
 
 cp fcrepo.war /usr/local/tomcat/webapps/fcrepo.war
-#   && chown -R $FCREPO_UID /usr/local/tomcat/webapps/fcrepo.war
+#chown -R $FCREPO_USER:$FCREPO_USER /usr/local/tomcat/webapps/fcrepo.war
+
+echo "--- sleep 25  --------------------------------"
+sleep 25
+
+cd $CATALINA_HOME
+chown -R $FCREPO_USER:$FCREPO_USER bin webapps temp logs work conf
+chmod -R 777 bin webapps temp logs work conf
+
 
 
 
@@ -33,13 +57,10 @@ cp fcrepo.war /usr/local/tomcat/webapps/fcrepo.war
 
 #-----------------------
 # build and add features
-cd /usr/local/tomcat
-  #  && chown -R $FCREPO_UID bin webapps temp logs work conf
-  #  #&& chmod -R 300 bin webapps temp logs work conf
 
 #RUN mkdir -p /usr/local/tomcat/conf/Catalina/localhost
 #RUN chown -R $FCREPO_UID /usr/local/tomcat/conf/Catalina
 #RUN chmod -R 740 /usr/local/tomcat/conf/Catalina
 #RUN chmod -R +s /usr/local/tomcat/conf/Catalina
 
-yes > /dev/null
+tail -f logs/catalina.*.log
