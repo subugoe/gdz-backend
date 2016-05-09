984de4f03cd6ce0898985fcbcfca3bf5036481bd
Joerg Panzer <panzer@sub.uni-goettingen.de>
2016-05-09 15:10:55 +0200

    check the startup status of the Apache Karaf runtime

--- docker/fcrepo/scripts/runall.sh
+++ docker/fcrepo/scripts/runall.sh
@@ -6,14 +6,25 @@ sed -i "s|#org.ops4j.pax.url.mvn.localRepository=|org.ops4j.pax.url.mvn.localRep
 #sed -i "s|# org.ops4j.pax.url.mvn.proxySupport=false|org.ops4j.pax.url.mvn.proxySupport=false|" /opt/karaf/etc/org.ops4j.pax.url.mvn.cfg
 
 
+
 /opt/karaf/bin/start
 
-sleep 90
+
+
+status=`/opt/karaf/bin/instance status root`
+st=`echo -n "$status" | grep  "Started"`
+
+while [ -z "$st" ]
+do
+    echo "Karaf not running yet"
+    sleep  5
+    status=`/opt/karaf/bin/instance status root`
+    st=`echo -n "$status" | grep  "Started"`
+done
+echo "Karaf is running"
 
 
 /fcrepo_config/fedora_camel_toolbox.sh
 
-
 cd /usr/local/tomcat
 sh /usr/local/tomcat/bin/catalina.sh run
-#sh catalina.sh run
