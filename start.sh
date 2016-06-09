1332132f2b3f0e44971d5cf5adaa1c60cf622acf
Joerg Panzer <panzer@sub.uni-goettingen.de>
2016-06-09 10:05:54 +0200

    Modify container start script

--- start.sh
+++ start.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
 
-if [ -z ${FCREPO_STORAGE} ]; then export FCREPO_STORAGE=/mnt/storage; fi
+
+# --- storage
+
+if [ -z ${FCREPO_STORAGE} ]; then export FCREPO_STORAGE=./data; fi
 
 mkdir -p $FCREPO_STORAGE/fcrepo
 mkdir -p $FCREPO_STORAGE/solr
 mkdir -p $FCREPO_STORAGE/redis
 mkdir -p $FCREPO_STORAGE/fuseki
+chmod -R 744 $FCREPO_STORAGE
 
 
-sed -i "s|    user: |    #user: |" docker-compose.yml
+# --- modify compose file, depending on the plattform
+
+unamestr=`uname`
+if [[ "$unamestr" == "Darwin"* ]]; then
+        sed -i.bak "s|    user: |    #user: |" docker-compose.yml
+else
+        sed -i "s|    user: |    #user: |" docker-compose.yml
+fi
+
 
 docker-compose build
 docker-compose up -d
